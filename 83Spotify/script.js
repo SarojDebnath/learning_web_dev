22d6ef4b3ce175ba007d5f9f88f6240c1e136665
SarojDebnath <ripandebnath2405@gmail.com>
2024-12-28 14:49:07 +0100

    Playlists

--- 83Spotify/script.js
+++ 83Spotify/script.js
@@ -3,31 +3,36 @@ async function getSongs() {
     let a = await fetch("http://127.0.0.1:3000/83Spotify/songs/Lakshya/");
     let response = await a.text();
     console.log(response);
-    let div=document.createElement("div")
-    div.innerHTML=response;
-    let as=div.getElementsByTagName("a")
-    let songs =[]
+    let div = document.createElement("div")
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+    let songs = []
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
-        if (element.href.endsWith("mp3")){
+        if (element.href.endsWith("mp3")) {
             songs.push(element.href.split("/Lakshya/")[1])
         }
     }
     return songs
 }
 async function main() {
-    let songs= await getSongs()
+    //get list of all songs
+    let songs = await getSongs()
     console.log(songs)
-    let songUL=document.querySelector(".songlist").getElementsByTagName("ul")[0]
+    let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0]
     for (const song of songs) {
-        songUL.innerHTML+=`<li>${song.replaceAll("-"," ")}</li>`
+        songUL.innerHTML += `<li>
+                            <img class="invert" src="img/music.svg" alt="">
+                            <div class="info">
+                                <div>${song.replaceAll("%20", " ")}</div>
+                                <div>Saroj</div>
+                            </div>
+                            <div class="playnow">
+                                <span>Play Now</span>
+                                <img class="invert" src="img/playbarplay.svg" alt="">
+                            </div></li>`
     }
-    var audio=new Audio(songs[0]);
-    //audio.play();
-
-    audio.addEventListener("loadeddata",()=>{
-        let duration=audio.duration;
-        console.log(duration)
-    })
 }
- main()
+
+
+main()
